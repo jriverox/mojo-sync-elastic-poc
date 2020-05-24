207d1647e4acfe75f3a26508aebe6579ac865f84
Jhony Rivero <jhony.rivero@hotmail.com>
2020-05-23 20:56:05 -0500

    insertr en la cola listo

--- data-reader/src/routes/revenue.route.js
+++ data-reader/src/routes/revenue.route.js
@@ -1,14 +1,19 @@
+const { performance } = require('perf_hooks')
 const KoaRouter = require('koa-router')
 const router = new KoaRouter({ prefix: '/revenue' })
 const RevenueSyncService = require('../services/revenue-sync.service')
 const syncService = new RevenueSyncService()
 
 router.put('sync-request', '/', async (ctx) => {
+  const startTime = performance.now()
   const request = ctx.request.body
-  console.log(request)
   // TODO: validar con joi
   await syncService.sync(request.readPageSize, request.startPage, request.endPage)
-  ctx.body = true
+  const duration = performance.now() - startTime
+  ctx.body = {
+    message: 'success',
+    duration: duration,
+  }
 })
 
 module.exports = router
--- data-reader/src/services/revenue-sync.service.js
+++ data-reader/src/services/revenue-sync.service.js
@@ -1,7 +1,7 @@
 const yenv = require('yenv')
 const RevenueRepository = require('../repositories/revenue.repository')
 const QueueRepository = require('../repositories/queue.repository')
-
+const utils = require('../utils/utils.js')
 const env = yenv()
 module.exports = class RevenueSyncService {
   constructor() {
@@ -9,25 +9,42 @@ module.exports = class RevenueSyncService {
     this.queueRepository = new QueueRepository(env.SQS.URL, env.SQS.REGION)
   }
 
-  async sync(readPageSize = 50, startPage = 0, endPage = 0) {
-    let sqsMessages = []
-    let sqsBatchCount = 0
-    console.log(`sync readPageSize: ${readPageSize}, startPage: ${startPage}, endPage: ${endPage}`)
+  async sync(pageSize = 50, startPage = 0, endPage = 0) {
+    console.log(`sync readPageSize: ${pageSize}, startPage: ${startPage}, endPage: ${endPage}`)
     for (let page = startPage; page <= endPage; page++) {
-      const revenues = await this.revenueRepository.getRevenues(readPageSize, page)
-      // console.log(revenues)
-      const message = {
-        header: `header-${page}`,
-        data: revenues,
-      }
+      const data = await this.tryGetRevenues(pageSize, page)
+      await this.sendToQueue(data, page)
+    }
+  }
 
-      sqsMessages.push(message)
-      sqsBatchCount += 1
-      console.log('for', sqsBatchCount, page, endPage)
-      if (sqsBatchCount === 10 || page === endPage) {
-        await this.queueRepository.sendBatch(sqsMessages)
-        sqsMessages = []
-        sqsBatchCount = 0
+  async tryGetRevenues(readPageSize, page, attempt = 1, msDelay = 500) {
+    try {
+      return await this.revenueRepository.getRevenues(readPageSize, page)
+    } catch (error) {
+      const retries = env.BACKOFF_RETRIES || 0
+      console.error(`Ha ocurrido un error, reintento ${attempt} de ${retries}, detalle: ${error}`)
+
+      if (attempt <= retries) {
+        attempt += 1
+        await utils.delay(msDelay)
+        return await this.tryGetRevenues(readPageSize, page, attempt, msDelay * 2)
+      }
+    }
+  }
+
+  async sendToQueue(data, page) {
+    if (data && data.length && data.length > 0) {
+      const maxBatchSize = env.SQS.MAX_BATCH_SIZE
+      const chunkedArray = utils.chunkArray(data, maxBatchSize)
+      console.log('chunkedArray devolvio', chunkedArray.length)
+      for (let index = 0; index < chunkedArray.length; index++) {
+        const singleArray = chunkedArray[index]
+        const message = {
+          header: `header-${page}-${index}`,
+          data: singleArray,
+        }
+        const id = await this.queueRepository.send(message)
+        console.log(id)
       }
     }
   }
